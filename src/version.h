7385b542e3226c9d29bbb31fe4b005f61c772a7b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-11-28 15:35:48 -0600

    Minor change: Added license info to version.h
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/version.h
+++ src/version.h
@@ -4,6 +4,12 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_REVISION 0
-#define VERSION_STRING "1.0.0"
+#define VERSION_STRING "full-text-search-1.0.0"
+
+#define LICENSE_STRING "Copyright (C) 2022 Sam Christy.\n" \
+"License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n" \
+"\n" \
+"This is free software; you are free to change and redistribute it.\n" \
+"There is NO WARRANTY, to the extent permitted by law."
 
 #endif
